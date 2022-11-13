a594548bc0a6b9a8c0b5b567c08a8398300dda9c
nikanyx <nikanyx@protonmail.com>
2022-11-13 23:31:21 +0000

    new logic

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cmachado <cmachado@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 19:42:34 by cmachado          #+#    #+#             */
-/*   Updated: 2022/09/29 19:54:08 by cmachado         ###   ########.fr       */
+/*   Updated: 2022/10/24 00:15:52 by cmachado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,13 @@ char	*get_next_line(int fd)
 	static char	buf[BUFFER_SIZE + 1];
 
 	data.temp = NULL;
-	if (fd < 0 || fd >= FOPEN_MAX || BUFFER_SIZE < 1)
+	if (fd < 0 || fd >= FOPEN_MAX || BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
 		return (NULL);
-	if (buf[0] != '\0')
-	{
-		data.temp = set_temp(data.temp, buf, check_nl(buf));
-		if (data.temp[0] == '\n' && data.temp[1] == '\0')
-			return (data.temp);
-	}
 	while (1)
 	{
-		data.cnt = read(fd, buf, BUFFER_SIZE);
-		data.nl = check_nl(buf);
-		data.flag = check_flag(buf, data.nl);
-		if (data.cnt > 0)
-			data.temp = set_temp(data.temp, buf, data.nl);
-		if (data.nl < BUFFER_SIZE || data.flag == 1)
-			return (data.temp);
+		if (buf[0] != 0)
+			
+		break;
 	}
 	return (data.temp);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,85 +6,23 @@
 /*   By: cmachado <cmachado@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 15:43:54 by cmachado          #+#    #+#             */
-/*   Updated: 2022/09/29 19:54:06 by cmachado         ###   ########.fr       */
+/*   Updated: 2022/10/24 00:15:50 by cmachado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	check_flag(char *buf, int nl)
+void cpybuf ()
 {
-	return (buf[nl - 1] == '\n');
-}
+	int i;
+	int j;
 
-int	check_nl(char *temp)
-{
-	int		i;
-
-	i = 0;
-	if (!temp)
-		return (0);
-	while (temp[i])
+	while (data.temp[j])
+		j++;
+	while (buf[i])
 	{
-		if (temp[i] == '\n')
-		{
-			i++;
-			break ;
-		}
-		i++;
+		data.temp[j] = buf[i];
+		if (data.temp[j] == '\n')
+			br
 	}
-	return (i);
-}
-
-int	set_buf(char *buf, int val)
-{
-	int	i;
-
-	i = 0;
-	while (i <= BUFFER_SIZE)
-	{
-		if (val <= BUFFER_SIZE)
-			((char *)buf)[i++] = ((char *)buf)[val++];
-		else
-			buf[i++] = '\0';
-	}
-	return (i);
-}
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t	i;
-
-	i = 0;
-	if (!dst && !src)
-		return (NULL);
-	while (i < n)
-	{
-		((char *)dst)[i] = ((char *)src)[i];
-		i++;
-	}
-	return (dst);
-}
-
-char	*set_temp(char *s1, char *s2, int nl)
-{
-	char	*temp;
-	int		s_s1;
-
-	temp = NULL;
-	s_s1 = 0;
-	if (s1)
-		s_s1 = check_nl(s1);
-	temp = (char *) malloc(nl + s_s1 + 1);
-	if (!temp)
-		return (NULL);
-	if (s1)
-	{
-		ft_memcpy(temp, s1, s_s1);
-		free(s1);
-	}
-	ft_memcpy(temp + s_s1, s2, nl);
-	temp[s_s1 + nl] = '\0';
-	set_buf(s2, nl);
-	return (temp);
 }
